141d816d83d95e1394bf43a34f353cbfffb063b9
Jeremy <JeremyPlott@gmail.com>
2019-09-17 11:05:50 -0400

    Add project files.

---
+++ PRS_ServerV2/Models/PRSDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRS_ServerV2.Models {
+
+    public class PRSDbContext : DbContext {
+        public PRSDbContext(DbContextOptions<PRSDbContext> context) : base(context) { }
+    }
+}
---
+++ PRS_ServerV2/Models/RequestLineModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRS_ServerV2.Models {
+
+    public partial class RequestLines {
+
+        public int Id { get; set; }
+        public int RequestId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        [ForeignKey("ProductId")]
+        [InverseProperty("RequestLines")]
+        public virtual Products Product { get; set; }
+        [ForeignKey("RequestId")]
+        [InverseProperty("RequestLines")]
+        [JsonIgnore] // I think this is where it's supposed to go to avoid circular reference?
+        public virtual Requests Request { get; set; }
+    }
+}
