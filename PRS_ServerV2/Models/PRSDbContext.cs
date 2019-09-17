94a0cd408974aeba80a37e533850645f2e9f4652
Jeremy <JeremyPlott@gmail.com>
2019-09-17 11:17:19 -0400

    Enough code to get the database created

--- PRS_ServerV2/Models/PRSDbContext.cs
+++ PRS_ServerV2/Models/PRSDbContext.cs
@@ -8,5 +8,11 @@ namespace PRS_ServerV2.Models {
 
     public class PRSDbContext : DbContext {
         public PRSDbContext(DbContextOptions<PRSDbContext> context) : base(context) { }
+
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<RequestLines> RequestLines { get; set; }
+        public virtual DbSet<Requests> Requests { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Vendors> Vendors { get; set; }
     }
 }
